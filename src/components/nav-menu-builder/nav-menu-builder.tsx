01aca03aec045f583fa4086f0739ab9daddc9122
Marcin Mosiejko <marcin@stratokit.io>
2024-12-06 09:26:20 +0100

    refactor: move move builder component parts to separate file

--- src/components/nav-menu-builder/nav-menu-builder.tsx
+++ src/components/nav-menu-builder/nav-menu-builder.tsx
@@ -1,13 +1,11 @@
 "use client";
 
 import { Button } from "@/components/button";
-import PlusIcon from "@/components/icons/plus-icon";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Form } from "@/components/form";
 import { FC, useCallback, useEffect, useState } from "react";
 import { useMenuItemsArray } from "./useMenuItemsArray";
-import { MenuItemBaseFields } from "./menu-item/menu-item-base-fields";
 import { buttonTextAndPadding, MenuItem } from "./menu-item/menu-item";
 import { FormSubmitButtons } from "../form-submit-buttons";
 import {
@@ -27,62 +25,12 @@ import { cn, getStateFromLocalStorage, runAtEndOfCallStack } from "@/lib/utils";
 import { DragOverlay } from "@dnd-kit/core";
 import { createPortal } from "react-dom";
 import { useHandleArrayFieldById } from "./context";
-import { Switch } from "../switch";
-import { Label } from "../label";
+import { DnDMode, MenuEmptyState, TopMenuItemFields } from "./components";
 
 export const STORAGE_KEY = "menuItems";
 export const DEPTH_LIMIT = 7;
 
 const topMenuItemPath = "" as MenuItemPath;
-
-const TopMenuItemFields = () => {
-  return (
-    <div className="bg-background border-border flex flex-col gap-2 rounded-md border p-6 md:gap-4">
-      <h2 className="font-semibold">Nazwa</h2>
-      <MenuItemBaseFields
-        nameFieldProps={{
-          label: "Menu",
-          placeholder: "np. Menu główne",
-          name: "name",
-        }}
-        linkFieldProps={{ name: "link" }}
-      />
-    </div>
-  );
-};
-
-const MenuEmptyState: FC<{ onAddItem: () => void }> = ({ onAddItem }) => {
-  return (
-    <div className="space-y-4 py-6 text-center">
-      <div className="flex flex-col items-center gap-1">
-        <span className="text-sm font-semibold">Menu jest puste</span>
-        <span className="text-xs">
-          W tym menu nie ma jeszcze żadnych linków
-        </span>
-      </div>
-      <Button variant="primary" onClick={onAddItem}>
-        <PlusIcon />
-        Dodaj pozycję menu
-      </Button>
-    </div>
-  );
-};
-
-const DnDMode: FC<{
-  value: boolean;
-  onChange: (v: boolean) => void;
-}> = ({ value, onChange }) => {
-  return (
-    <div className="flex h-6 w-full items-center justify-between space-x-2 md:h-auto md:w-auto md:justify-normal">
-      <Label htmlFor="mode">
-        {value
-          ? "Pełny D&D (zbugowany)"
-          : "Okrojony D&D (jeden poziom, działa w większości przypadków)"}
-      </Label>
-      <Switch id="mode" checked={value} onCheckedChange={onChange} />
-    </div>
-  );
-};
 
 export const NavMenuBuilder = () => {
   const [isFullDnD, setIsFullDnD] = useState(false);
