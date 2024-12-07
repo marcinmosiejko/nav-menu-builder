99d3e304f125aef0a0acaa4eb73909586bcf1f9e
Marcin Mosiejko <marcin@stratokit.io>
2024-12-07 12:33:17 +0100

    feat: working drag & drop using zustand store

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@/components/button";
 import ArrowLeftIcon from "@/components/icons/arrow-left-icon";
 import { NavMenuBuilderProvider } from "@/components/nav-menu-builder/context";
+import { NavMenuBuilderProvider as NavMenuBuilderProvider2 } from "@/components/nav-menu-builder2/context";
 import { NavMenuBuilder } from "@/components/nav-menu-builder/nav-menu-builder";
+import { NavMenuBuilder2 } from "@/components/nav-menu-builder2/nav-menu-builder";
 
 export default function NavMenuBuilderPage() {
   return (
@@ -15,6 +17,9 @@ export default function NavMenuBuilderPage() {
       <NavMenuBuilderProvider>
         <NavMenuBuilder />
       </NavMenuBuilderProvider>
+      <NavMenuBuilderProvider2>
+        <NavMenuBuilder2 />
+      </NavMenuBuilderProvider2>
     </div>
   );
 }
