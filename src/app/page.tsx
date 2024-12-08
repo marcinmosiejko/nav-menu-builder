fb7f3faaddf329ddf336ab9f8d1caf20961ce73f
Marcin Mosiejko <marcin@stratokit.io>
2024-12-08 23:51:05 +0100

    refactor: remove code for previous nav menu builder that didn't work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,6 @@
 import { Button } from "@/components/button";
 import ArrowLeftIcon from "@/components/icons/arrow-left-icon";
-import { NavMenuBuilderProvider } from "@/components/nav-menu-builder/context";
 import { NavMenuBuilderProvider as NavMenuBuilderProvider2 } from "@/components/nav-menu-builder2/context";
-import { NavMenuBuilder } from "@/components/nav-menu-builder/nav-menu-builder";
 import { NavMenuBuilder2 } from "@/components/nav-menu-builder2/nav-menu-builder";
 
 export default function NavMenuBuilderPage() {
@@ -14,9 +12,7 @@ export default function NavMenuBuilderPage() {
           Wróć do listy nawigacji
         </Button>
       </nav>
-      {/* <NavMenuBuilderProvider>
-        <NavMenuBuilder />
-      </NavMenuBuilderProvider> */}
+
       <NavMenuBuilderProvider2>
         <NavMenuBuilder2 />
       </NavMenuBuilderProvider2>
